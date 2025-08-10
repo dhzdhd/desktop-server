1c95c29acfbe84ef50bce94e905f78e33a5b61ca
dhzdhd <dhzdhd@proton.me>
2025-08-11 02:01:05 +0530

    Remove unnecessary outputs

--- iac/opentofu/outputs.tf
+++ iac/opentofu/outputs.tf
@@ -5,12 +5,3 @@ output "vm_public_ip" {
 output "ssh_command" {
   value = "ssh -i ${var.priv_key_path} ${var.user}@${azurerm_public_ip.public_ip.ip_address}"
 }
-
-output "log" {
-  value = ansible_playbook.local.temp_inventory_file
-}
-
-
-output "logg" {
-  value = ansible_playbook.server.temp_inventory_file
-}
