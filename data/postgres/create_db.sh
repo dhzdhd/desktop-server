b39068fe1ade0e280c935b67cb3265b12377620f
dhzdhd <dhzdhd@proton.me>
2026-07-10 02:26:23 +0530

    Add database explicitly

--- data/postgres/create_db.sh
+++ data/postgres/create_db.sh
@@ -1,12 +1,8 @@
-#!/bin/bash
-set -e
-set -u
-
 function create_user_and_database() {
 	local database=$1
-	echo "  Ensuring role and database '$database' exist"
+	echo "Ensuring role and database '$database' exist"
 
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" <<-EOSQL
 	    DO \$\$
 	    BEGIN
 	       IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$database') THEN
@@ -16,27 +12,16 @@ function create_user_and_database() {
 	    \$\$;
 EOSQL
 
-	if ! psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -lqt | cut -d '|' -f 1 | grep -qw "$database"; then
-	    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE DATABASE $database OWNER $POSTGRES_USER;"
+	if ! psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" -lqt | cut -d '|' -f 1 | grep -qw "$database"; then
+	    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE DATABASE $database OWNER $POSTGRES_USER;"
 	fi
 
-	# Both roles get full privileges on the database
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "GRANT ALL PRIVILEGES ON DATABASE $database TO $database;"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" -c "GRANT ALL PRIVILEGES ON DATABASE $database TO $database;"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$POSTGRES_DB" -c "GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;"
 
-	# Schema-level grants (needed on PG15+, public schema is no longer world-writable by default)
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$database" -c "GRANT ALL ON SCHEMA public TO $database;"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$database" -c "GRANT ALL ON SCHEMA public TO $POSTGRES_USER;"
 
-	# Make sure future tables/sequences created by either role are usable by both
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$database" -c "ALTER DEFAULT PRIVILEGES FOR ROLE $POSTGRES_USER IN SCHEMA public GRANT ALL ON TABLES TO $database;"
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$database" -c "ALTER DEFAULT PRIVILEGES FOR ROLE $database IN SCHEMA public GRANT ALL ON TABLES TO $POSTGRES_USER;"
 }
-
-if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
-	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
-	for db in $(echo "$POSTGRES_MULTIPLE_DATABASES" | tr ',' ' '); do
-		create_user_and_database "$db"
-	done
-	echo "Multiple databases created/verified"
-fi
